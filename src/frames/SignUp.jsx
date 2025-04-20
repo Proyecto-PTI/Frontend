86ffadb12243e5ea2e6416e24f7966990210d688
yolanda-rdc <yoli.rdc@gmail.com>
2025-04-20 22:56:10 +0200

    SignUp done

--- src/frames/SignUp.jsx
+++ src/frames/SignUp.jsx
@@ -1,8 +1,88 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./SignUp.module.css";
 
 function SignUp() {
-    return <h2>SignUp</h2>;
-  }
-  
-  export default SignUp;
-  
+  return (
+    <section className={styles.signup}>
+
+      <div className={styles.header}>
+        <h1 className={styles.facepass}>FACEPASS</h1>
+        <h2 className={styles.createaNewAdminAccount}>
+          Create a New Admin Account
+        </h2>
+      </div>
+
+
+      <form 
+        className={styles.signupForm}
+      >
+        <div className={styles.FormInput}>
+          <label className={styles.userName}>User Name</label>
+          <div className={styles.userbox}>
+            <input
+              type="text"
+              placeholder="yourusername..."
+              className={styles.inputField}
+            />
+          </div>
+        </div>
+
+        <div className={styles.FormInput}>
+          <label className={styles.email}>Email</label>
+          <div className={styles.emailbox}>
+            <input
+              type="email"
+              placeholder="youremail@email.com..."
+              className={styles.inputField}
+            />
+          </div>
+        </div>
+
+        <div className={styles.FormInput}>
+          <label className={styles.password}>Password</label>
+          <div className={styles.passwordbox}>
+            <input
+              type="password"
+              placeholder="yourpassword..."
+              className={styles.inputField}
+            />
+          </div>
+        </div>
+
+        <div className={styles.FormInput}>
+          <label className={styles.confirmPassword}>Confirm Password</label>
+          <div className={styles.passwordbox2}>
+            <input
+              type="password"
+              placeholder="yourpassword..."
+              className={styles.inputField}
+            />
+          </div>
+        </div>
+
+      </form>
+
+
+      <div className={styles.footer}>
+
+        <button type="button" className={styles.signupbutton}>
+          Sign up
+        </button>
+
+        <p className={styles.alreadyhaveanaccount}>
+          Already have an account?{"  "}
+          <Link to="/login" className={styles.loginLink}>
+              Log in
+          </Link>{"  "}
+          <span className={styles.loginText}>to your admin </span>
+        </p>
+
+      </div>
+
+
+    </section>
+  );
+}
+
+export default SignUp;
