5ef05cc3a54eb1856bc1055722c99aba257f679b
yolanda-rdc <-v>
2025-04-27 20:25:17 +0200

    fake Backend Centralizado, nuevos comandos para ejecutar

--- src/api/dashboardAPI.js
+++ src/api/dashboardAPI.js
@@ -1,43 +1,43 @@
 // src/api/dashboardAPI.js
 
 export async function fetchAuthorizedAccess() {
-  const response = await fetch("http://localhost:3001/api/authorized-access");
+  const response = await fetch("http://localhost:5000/api/authorized-access");
   if (!response.ok) throw new Error("Failed to fetch authorized access");
   return await response.json();
 }
 
 export async function fetchDeniedAttempts() {
-  const response = await fetch("http://localhost:3001/api/denied-attempts");
+  const response = await fetch("http://localhost:5000/api/denied-attempts");
   if (!response.ok) throw new Error("Failed to fetch denied attempts");
   return await response.json();
 }
 
 export async function fetchPeakHour() {
-  const response = await fetch("http://localhost:3001/api/peak-hour");
+  const response = await fetch("http://localhost:5000/api/peak-hour");
   if (!response.ok) throw new Error("Failed to fetch peak hour");
   return await response.json();
 }
 
 export async function fetchMostAccessedDoor() {
-  const response = await fetch("http://localhost:3001/api/most-accessed-door");
+  const response = await fetch("http://localhost:5000/api/most-accessed-door");
   if (!response.ok) throw new Error("Failed to fetch most accessed door");
   return await response.json();
 }
 
 export async function fetchHourlyAccess() {
-  const response = await fetch("http://localhost:3001/api/hourly-access");
+  const response = await fetch("http://localhost:5000/api/hourly-access");
   if (!response.ok) throw new Error("Failed to fetch hourly access");
   return await response.json();
 }
 
 export async function fetchWeeklyEvolution() {
-  const response = await fetch("http://localhost:3001/api/weekly-evolution");
+  const response = await fetch("http://localhost:5000/api/weekly-evolution");
   if (!response.ok) throw new Error("Failed to fetch weekly evolution");
   return await response.json();
 }
 
 export async function fetchRecentRecords() {
-  const response = await fetch("http://localhost:3001/api/recent-records");
+  const response = await fetch("http://localhost:5000/api/recent-records");
   if (!response.ok) throw new Error("Failed to fetch recent records");
   return await response.json();
 }
