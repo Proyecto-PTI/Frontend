504a8031adb724cea18d6f6fb76f40a2c3446500
mar.puigmarti <mar.puigmarti@estudiantat.upc.edu>
2025-05-13 11:32:14 +0200

    codigo demo

--- src/frames/AddUser.jsx
+++ src/frames/AddUser.jsx
@@ -96,8 +96,8 @@ const handleCreateUser = async () => {
       formData.append("images", renamedFile);
     });
 
-    const response = await fetch("http://backend-service:8000/new-user", {
-    //const response = await fetch("http://localhost:8000/new-user", {
+    //const response = await fetch("http://backend-service:8000/new-user", {
+    const response = await fetch("http://localhost:8000/new-user", {
       method: "POST",
       body: formData,
     });
